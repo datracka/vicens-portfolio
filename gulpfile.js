47d70acc2b91ad45791c6359600e79591a92441d
Vicens Fayos <datracka@gmail.com>
2015-12-11 15:07:03 +0100

    write gulp style and add all the dependencies needed

---
+++ gulpfile.js
@@ -0,0 +1,133 @@
+var argv         = require('minimist')(process.argv.slice(2));
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync  = require('browser-sync').create();
+var changed      = require('gulp-changed');
+var concat       = require('gulp-concat');
+var flatten      = require('gulp-flatten');
+var gulp         = require('gulp');
+var gulpif       = require('gulp-if');
+var imagemin     = require('gulp-imagemin');
+var jshint       = require('gulp-jshint');
+var lazypipe     = require('lazypipe');
+var less         = require('gulp-less');
+var merge        = require('merge-stream');
+var minifyCss    = require('gulp-minify-css');
+var plumber      = require('gulp-plumber');
+var rev          = require('gulp-rev');
+var runSequence  = require('run-sequence');
+var sass         = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
+var uglify       = require('gulp-uglify');
+
+// See https://github.com/austinpray/asset-builder
+var manifest = require('asset-builder')('./app/assets/manifest.json');
+//paths to assets directories
+var path = manifest.paths;
+var config = manifest.config || {};
+var project = manifest.getProjectGlobs();
+
+// CLI options
+var enabled = {
+    // Enable static asset revisioning when `--production`
+    rev: argv.production,
+    // Disable source maps when `--production`
+    maps: !argv.production,
+    // Fail styles task on error when `--production`
+    failStyleTask: argv.production,
+    // Fail due to JSHint warnings only when `--production`
+    failJSHint: argv.production,
+    // Strip debug statments from javascript when `--production`
+    stripJSDebug: argv.production
+};
+
+var cssTasks = function(filename) {
+    return lazypipe()
+        .pipe(function() {
+            return gulpif(!enabled.failStyleTask, plumber());
+        })
+        .pipe(function() {
+            return gulpif(enabled.maps, sourcemaps.init());
+        })
+        .pipe(function() {
+            return gulpif('*.less', less());
+        })
+        .pipe(function() {
+            return gulpif('*.scss', sass({
+                outputStyle: 'nested', // libsass doesn't support expanded yet
+                precision: 10,
+                includePaths: ['.'],
+                errLogToConsole: !enabled.failStyleTask
+            }));
+        })
+        .pipe(concat, filename)
+        .pipe(autoprefixer, {
+            browsers: [
+                'last 2 versions',
+                'android 4',
+                'opera 12'
+            ]
+        })
+        .pipe(minifyCss, {
+            advanced: false,
+            rebase: false
+        })
+        .pipe(function() {
+            return gulpif(enabled.rev, rev());
+        })
+        .pipe(function() {
+            return gulpif(enabled.maps, sourcemaps.write('.', {
+                sourceRoot: 'app/assets/styles/'
+            }));
+        })();
+};
+
+// Path to the compiled assets manifest in the dist directory
+var revManifest = path.dist + 'assets.json';
+
+// If there are any revved files then write them to the rev manifest.
+// See https://github.com/sindresorhus/gulp-rev
+var writeToManifest = function(directory) {
+    return lazypipe()
+        .pipe(gulp.dest, path.dist + directory)
+        .pipe(browserSync.stream, {match: '**/*.{js,css}'})
+        .pipe(rev.manifest, revManifest, {
+            base: path.dist,
+            merge: true
+        })
+        .pipe(gulp.dest, path.dist)();
+};
+
+/** tasks **/
+
+gulp.task('styles', ['wiredep'], function() {
+    var merged = merge();
+    manifest.forEachDependency('css', function(dep) {
+        var cssTasksInstance = cssTasks(dep.name);
+        if (!enabled.failStyleTask) {
+            cssTasksInstance.on('error', function(err) {
+                console.error(err.message);
+                this.emit('end');
+            });
+        }
+        merged.add(gulp.src(dep.globs, {base: 'styles'})
+            .pipe(cssTasksInstance));
+    });
+    return merged
+        .pipe(writeToManifest('styles'));
+});
+
+
+
+// ### Wiredep
+// `gulp wiredep` - Automatically inject Less and Sass Bower dependencies. See
+// https://github.com/taptapship/wiredep
+gulp.task('wiredep', function() {
+    var wiredep = require('wiredep').stream;
+    return gulp.src(project.css)
+        .pipe(wiredep())
+        .pipe(changed(path.source + 'styles', {
+            hasChanged: changed.compareSha1Digest
+        }))
+        .pipe(gulp.dest(path.source + 'styles'));
+});
+
